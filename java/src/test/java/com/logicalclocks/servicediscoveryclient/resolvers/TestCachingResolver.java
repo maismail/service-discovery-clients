117504793cda78f76c0c7c9c1b970a70d6811efa
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-03-26 08:59:35 +0100

    Change license headers

--- java/src/test/java/com/logicalclocks/servicediscoveryclient/resolvers/TestCachingResolver.java
+++ java/src/test/java/com/logicalclocks/servicediscoveryclient/resolvers/TestCachingResolver.java
@@ -1,3 +1,18 @@
+/**
+ * This file is part of service-discovery-clients
+ * Copyright (C) 2020, Logical Clocks AB. All rights reserved
+ *
+ * service-discovery-clients is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * service-discovery-clients is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.logicalclocks.servicediscoveryclient.resolvers;
 
 import com.logicalclocks.servicediscoverclient.Builder;
